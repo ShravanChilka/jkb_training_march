66cd733d0d8e5feaa73470960694a41c882c04bd
shravanchilka <shravanchilka16@gmail.com>
2024-04-23 22:47:44 +0530

    art board screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:jkb_training_march/screens/counter_screen.dart';
+import 'package:jkb_training_march/pixel_art/pixel_art_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData.light(useMaterial3: true),
-      home: const CounterScreen(),
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const PixelArtScreen(),
     ),
   );
 }
---
+++ lib/pixel_art/pixel_art_screen.dart
@@ -0,0 +1,113 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:jkb_training_march/pixel_art/artworks.dart';
+
+const totalRows = 24;
+const totalColumns = 24;
+
+class PixelArtScreen extends StatefulWidget {
+  const PixelArtScreen({super.key});
+
+  @override
+  State<PixelArtScreen> createState() => _PixelArtScreenState();
+}
+
+class _PixelArtScreenState extends State<PixelArtScreen> {
+  late List<Color?> pixels;
+
+  late List<Color> palette;
+  late Color selectedColor;
+
+  @override
+  void initState() {
+    super.initState();
+    pixels = List.filled(
+      totalRows * totalColumns,
+      null,
+    );
+    palette = [
+      const Color(0xFFC6C6C6),
+      const Color(0xFFFE0652),
+      const Color(0xFF080101),
+      const Color(0xFFF9A332),
+      const Color(0xFFFF6D34),
+      const Color(0xFFD04D5D),
+    ];
+    selectedColor = palette[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pixel Art'),
+      ),
+      body: Stack(
+        children: [
+          GridView.builder(
+            physics: const ClampingScrollPhysics(),
+            itemCount: pixels.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: totalRows,
+            ),
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  setState(() {
+                    pixels[index] = selectedColor;
+                  });
+                  log(pixels.toString());
+                },
+                child: Ink(
+                  decoration: BoxDecoration(
+                    color: pixels[index],
+                    border: Border.all(
+                      width: duckArtPixels[index] != null ? 1 : 0.4,
+                      color: duckArtPixels[index] != null
+                          ? duckArtPixels[index]!
+                          : Colors.white10,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 16,
+            child: SizedBox(
+              height: 40,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: palette.map((color) {
+                  bool isSelected = color == selectedColor;
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedColor = color;
+                      });
+                    },
+                    child: Ink(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: color,
+                        border: Border.all(
+                          width: isSelected ? 2 : 1,
+                          color: isSelected ? Colors.white : Colors.white10,
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
